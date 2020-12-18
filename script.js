1246d9bbb6f9f896e84f1e246fd53f1821c5051a
Chris Scott <cam022007@coderacademy.edu.au>
2020-12-17 20:28:31 -0800

    the number randomly changes colour now! however I need to add some css because it's annoying as it affects the button size too

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 let i = 0
 
-// incrementor section 
+// this is targeting the value which I want to change - which is the place where the numbers are
 let x = document.getElementById("display-incrementor")
 
+// incrementor section 
 let increment = document.getElementById("increment");
 increment.addEventListener("click", function() {
   i ++;
   console.log(i);
   x.innerHTML = i;
   
+  // changes font size randomly
   x.style.fontSize = Math.floor((Math.random() * 12) + 10)+"px";
+
+  x.style.color = changeColor();
 })
 
 // decrementor section
@@ -20,6 +24,15 @@ decrement.addEventListener("click", function() {
   console.log(i);
   x.innerHTML = i;
 
+  // changes font size randomly
   x.style.fontSize = Math.floor((Math.random() * 12) + 10)+"px";
+  
+  // changes color size randomly 
+  x.style.color = changeColor();
 })
 
+
+// the function that actually gets a random color from the hex system
+function changeColor() {
+  return '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1, 6);
+}
