dcc17be9163b7e3892e459ffb2b8c86cd758bae1
Chris Scott <cam022007@coderacademy.edu.au>
2020-12-17 20:20:35 -0800

    font will now randomly change size when you increment and decrement, which is cool i guess

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let i = 0
 
+// incrementor section 
 let x = document.getElementById("display-incrementor")
 
 let increment = document.getElementById("increment");
@@ -7,13 +8,18 @@ increment.addEventListener("click", function() {
   i ++;
   console.log(i);
   x.innerHTML = i;
+  
+  x.style.fontSize = Math.floor((Math.random() * 12) + 10)+"px";
 })
 
+// decrementor section
 let decrement = document.getElementById("decrement");
 
 decrement.addEventListener("click", function() {
   i --;
   console.log(i);
   x.innerHTML = i;
+
+  x.style.fontSize = Math.floor((Math.random() * 12) + 10)+"px";
 })
 
