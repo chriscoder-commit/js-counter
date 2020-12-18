890c232bb9ba81272500de7592a434ec408c9cf9
Chris Scott <cam022007@coderacademy.edu.au>
2020-12-17 18:25:52 -0800

    incrementor and decrementor now working

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("working?");
+let i = 0
+
+let x = document.getElementById("display-incrementor")
+
+let increment = document.getElementById("increment");
+increment.addEventListener("click", function() {
+  i ++;
+  console.log(i);
+  x.innerHTML = i;
+})
+
+let decrement = document.getElementById("decrement");
+
+decrement.addEventListener("click", function() {
+  i --;
+  console.log(i);
+  x.innerHTML = i;
+})
+
